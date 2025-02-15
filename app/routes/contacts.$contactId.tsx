03099e87c111bce0971b8a2aab06ab31745756bf
Venus Ang <venus@users.noreply.github.com>
2025-02-14 22:22:11 -0800

    🔧(app/routes/contacts.$contactId.tsx): connect useLoaderData for contact var

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,18 +1,19 @@
-import { Form } from "@remix-run/react";
+import {json} from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
 
 import type { ContactRecord } from "../data";
 
-export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placecats.com/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+import { getContact } from "../data";
 
+
+export const loader = async ({ params }) => {
+  const contact = await getContact(params.contactId);
+  return json({ contact });
+};
+
+export default function Contact() {
+  const { contact } = useLoaderData<typeof loader>();
   return (
     <div id="contact">
       <div>
