789e50b22b8ea8400d314666f556431a8dcc9cb9
Venus Ang <venus@users.noreply.github.com>
2025-02-15 08:46:45 -0800

    ⭐(app/root.tsx): add redirect to edit page when adding new contact

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import type { LinksFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import {
   Form,
   Link,
@@ -16,7 +16,7 @@ import { createEmptyContact, getContacts } from "./data";
 
 export const action=async()=>{
   const contact = await createEmptyContact();
-  return json ({ contact});
+  return redirect(`/contacts/${contact.id}/edit`)
 };
 
 export const links: LinksFunction =()=> [
