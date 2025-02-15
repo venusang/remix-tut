b4b2e60e092c79841ee0e3b26b21ad12b2728953
Venus Ang <venus@users.noreply.github.com>
2025-02-15 09:14:21 -0800

    ⭐(app/routes/contacts.$contactId_.destroy.tsx): add destroy route

---
+++ app/routes/contacts.$contactId_.destroy.tsx
@@ -0,0 +1,18 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { redirect} from "@remix-run/node";
+import invariant from "tiny-invariant";
+
+import { deleteContact } from "../data";
+
+
+export const action = async({
+  params, 
+}: ActionFunctionArgs)=>{
+  invariant(params.contactId, "Missing contactId param");
+  await deleteContact(params.contactId);
+  return redirect(`/`);
+};
+
+export default function DeleteContact(){
+  
+}
