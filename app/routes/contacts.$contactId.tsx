eb8404e8aecd76bb5b015f99e687ac33363a6419
Venus Ang <venus@users.noreply.github.com>
2025-02-15 08:07:25 -0800

    refactor(app/routes/contacts.$contactId.tsx):validate params and throw response

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,14 +1,20 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import {json} from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
-
+import invariant from "tiny-invariant";
 import type { ContactRecord } from "../data";
 
 import { getContact } from "../data";
 
 
-export const loader = async ({ params }) => {
+
+export const loader = async ({ params ,} : LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing cnotactId param");
   const contact = await getContact(params.contactId);
+  if(!contact){
+    throw new Response("Not Found", { status: 404});
+  }
   return json({ contact });
 };
 
