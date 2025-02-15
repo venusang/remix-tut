50c407575b271805bd1cdcd8d985fba2318014ea
Venus Ang <venus@users.noreply.github.com>
2025-02-15 09:43:47 -0800

    ⭐(app/root.tsx): change search to onChange

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,7 @@ import {
   ScrollRestoration,
   useLoaderData,
   useNavigation,
+  useSubmit,
 } from "@remix-run/react";
 
 import appStylesHref from "./app.css?url";
@@ -37,6 +38,7 @@ export const loader = async ({
 export default function App() {
   const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation();
+  const submit = useSubmit();
 
   useEffect(()=> {
     const searchField = document.getElementById("q");
@@ -57,7 +59,12 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form 
+              id="search-form" 
+              onChange={(event)=>
+                submit(event.currentTarget)
+              }
+              role="search">
               <input
                 id="q"
                 aria-label="Search contacts"
